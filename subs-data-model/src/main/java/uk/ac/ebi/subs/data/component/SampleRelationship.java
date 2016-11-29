5a1a4dace48f2e005794d0beb3ae2a430fca0069
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-29 10:27:05 +0000

    Update SampleRelationship.java

--- subs-data-model/src/main/java/uk/ac/ebi/subs/data/component/SampleRelationship.java
+++ subs-data-model/src/main/java/uk/ac/ebi/subs/data/component/SampleRelationship.java
@@ -3,14 +3,30 @@ package uk.ac.ebi.subs.data.component;
 
 import uk.ac.ebi.subs.data.submittable.Sample;
 
-public class SampleRelationship extends SampleRef {
-    String relationshipNature; // e.g. Child of
+public class SampleRelationship {
+    private String relationshipNature; // e.g. Child of
+    private String relationshipTargetAccession; // e.g. SAMEA12345
+    private String relationshipTargetUUID; // e.g. SAMEA12345
 
+    public SampleRelationship(String relationshipNature, String relationshipTargetUUID) {
+        this.relationshipNature = relationshipNature;
+        this.relationshipTargetUUID = relationshipTargetUUID;
+    }
+    
+    public SampleRelationship(String relationshipNature, String relationshipTargetAccession) {
+        this.relationshipNature = relationshipNature;
+        this.relationshipTargetAccession = relationshipTargetAccession;
+    }
+    
     public String getRelationshipNature() {
         return relationshipNature;
     }
-
-    public void setRelationshipNature(String relationshipNature) {
-        this.relationshipNature = relationshipNature;
+    
+    public String getRelationshipTargetAccession() {
+        return relationshipTargetAccession;
+    }
+    
+    public String getRelationshipTargeUUID() {
+        return relationshipTargeUUID;
     }
 }
